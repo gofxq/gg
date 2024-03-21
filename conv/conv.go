5586a2f3d2fa14a1bf9dd3ec2892f5703b6a69af
gofxq <108152043+gofxq@users.noreply.github.com>
2024-03-21 16:53:11 +0800

    add must trans

--- conv/conv.go
+++ conv/conv.go
@@ -10,3 +10,20 @@ func Trans[T any](from any, to *T) error {
 	err = sonic.Unmarshal(b, to)
 	return err
 }
+
+func MustTrans[T any](from any) (to *T) {
+	if from == nil {
+		return nil
+	}
+
+	var t T
+	b, err := sonic.Marshal(from)
+	if err != nil {
+		return nil
+	}
+	err = sonic.Unmarshal(b, &t)
+	if err != nil {
+		return nil
+	}
+	return &t
+}
